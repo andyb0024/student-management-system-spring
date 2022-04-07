596eeacc552cc5cc206174e2f2c56c3a19131729
andyb0024 <andy.benage0024@gmail.com>
2022-04-07 09:22:23 -0700

    create student

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -16,14 +18,24 @@ public class StudentController {
     private StudentService studentService;
 
     @GetMapping("/students")
-    public String studentList(Model model){
-        List<Student> studentList=studentService.displayStudents();
-        model.addAttribute("studentList",studentList);
+    public String studentList(Model model) {
+        List<Student> studentList = studentService.displayStudents();
+        model.addAttribute("studentList", studentList);
         return "students/student-list";
     }
+
     @GetMapping("/students/add")
-    public String addStudent(Model model){
+    public String addStudentForm(Model model) {
         model.addAttribute("student", new Student());
         return "students/add-student";
+    }
+
+    @PostMapping("/students/save")
+    public String saveStudent(Student student, RedirectAttributes ra) {
+
+        studentService.saveStudent(student);
+        ra.addFlashAttribute("message", "Student has been added successfully");
+
+        return "redirect:";
     }
 }
